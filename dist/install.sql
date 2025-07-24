cdd6a7fcc95abdc1c826001e1ff06c888f5b8f40
Prabhakar <prabhakar.lnu@oracle.com>
2025-07-24 22:10:49 +0530

    release 1.0.0-11--add properties_file and change install.sql

--- dist/install.sql
+++ dist/install.sql
@@ -17,6 +17,7 @@ set sqlblanklines on
 
 -- Kick off Liquibase
 prompt "Installing/updating schemas"
-lb update -log -changelog-file releases/main.changelog.xml -search-path "."
+--lb update -log -changelog-file releases/main.changelog.xml -search-path "."
+lb update -log -changelog-file releases/main.changelog.xml -search-path "." -defaults-file &DEFAULTS_FILE
 
 --@utils/recompile.sql
